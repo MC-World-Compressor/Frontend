8bbb0baf99a5802e0f31dd3401dbc65de2a870b0
Papela <losgamerbuey@gmail.com>
2026-03-30 15:18:08 +0200

    Actualizando dependencias y pequeñas mejoras

--- app/[locale]/upload/page.js
+++ app/[locale]/upload/page.js
@@ -236,7 +236,10 @@ export default function HomePage({ params }) {
 
           <button 
             type="button" 
-            onClick={() => !subiendo && document.getElementById('mundo_comprimido').click()}
+            onClick={(e) => {
+              e.stopPropagation();
+              if (!subiendo) document.getElementById('mundo_comprimido').click();
+            }}
             disabled={subiendo}
             className={`cursor-pointer bg-gray-200 dark:bg-gray-600 hover:bg-gray-300 dark:hover:bg-gray-700 text-gray-800 dark:text-gray-200 py-2 px-6 rounded transition-colors w-48 ${subiendo ? 'opacity-50 cursor-not-allowed' : ''}`}
           >
